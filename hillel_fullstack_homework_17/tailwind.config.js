5efefbd1651eb573d17f5159c2f499edeb1b51de
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-01-07 22:25:02 +0200

    hw17 very beginning

--- hillel_fullstack_homework_17/tailwind.config.js
+++ hillel_fullstack_homework_17/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html, ./pages/*.html"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color__gift: "#ff5533",
+        color__tree: "#33ff33",
+        color__garland: "#3333ff",
+      },
+    },
   },
   plugins: [],
 }
