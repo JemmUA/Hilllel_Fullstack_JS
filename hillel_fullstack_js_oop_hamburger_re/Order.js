da6bc415a9007ba46711c284f481cb364d88812b
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-02-09 22:41:56 +0200

    hamburger. fixed getters/setters

--- hillel_fullstack_js_oop_hamburger_re/Order.js
+++ hillel_fullstack_js_oop_hamburger_re/Order.js
@@ -1,28 +1,31 @@
 export class Order {
+  #name = '';
+  #price = '';
+  #calories = '';
   constructor(name, price, calories) {
-    this.name = name;
-    this.price = price;
-    this.calories = calories;
+    this.#name = name;
+    this.#price = price;
+    this.#calories = calories;
   }
-  // set name(name){
-  //   this.name = name;
-  // }
-  // get name() {
-  //   console.log(this.name)
-  //   return this.name;
-  // }
-  // get price() {
-  //   return this.price;
-  // }
-  // get calories() {
-  //   return this.calories;
-  // }
-  //
+  set name(name){
+    this.#name = name;
+  }
+  get name() {
+    console.log(this.#name)
+    return this.#name;
+  }
+  get price() {
+    return this.#price;
+  }
+  get calories() {
+    return this.#calories;
+  }
 
-  // set price(price){
-  //   this.price = price;
-  // }
-  // set calories(calories){
-  //   this.calories = calories;
-  // }
+
+  set price(price){
+    this.#price = price;
+  }
+  set calories(calories){
+    this.#calories = calories;
+  }
 }
