c0609c52c30814a71d4eb469ff2f3fe0866ffe92
JemmUA <jemm.ua@gmail.com>
2025-02-16 22:27:31 +0200

    hw27 switchers ok

--- hillel_fullstack_homework_27/carrousel_script.js
+++ hillel_fullstack_homework_27/carrousel_script.js
@@ -11,15 +11,15 @@ const demonstratorElement = document.getElementById("demonstrator");
 console.log("Demonstrator:", demonstratorElement, "wsidth =", demonstratorElement.getBoundingClientRect().width);
 
 const imagesContainerElement = document.getElementById("images-container")
-console.log("Images container", imagesContainerElement, imagesContainerElement.offsetWidth);
+console.log("Images container", imagesContainerElement, imagesContainerElement.getBoundingClientRect().width);
 
 const imageElements = document.getElementsByTagName("img");
-console.log("Images amount = ", imageElements.length, "width = ", imageElements[0].offsetWidth);
+console.log("Images amount = ", imageElements.length, "width = ", imageElements[demonstratingImage].getBoundingClientRect().width);
 
-function moveLeft() {
+function moveRight() {
   console.log("moving left");
   if (demonstratingImage === imageElements.length - 1) {
-    setTimeout(moveLeft, 0);
+    setTimeout(moveRight, 0);
     imagesContainerElement.style.transition = 'transform 0s';
   } else {
     imagesContainerElement.style.transition = 'transform 1s';
@@ -31,14 +31,15 @@ function moveLeft() {
   }
   console.log("Image now:", demonstratingImage);
   imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
-  console.log("Images container coords:", - demonstratingImage * demonstratorElement.getBoundingClientRect().width);
+  // console.log("Images container coords:", - demonstratingImage * demonstratorElement.getBoundingClientRect().width);
+  switcherTurnOn(demonstratingImage);
 }
 
-function moveRight() {
+function moveLeft() {
   console.log("moving right");
   if (demonstratingImage === 0) {
     imagesContainerElement.style.transition = 'transform 0s';
-    setTimeout(moveRight, 0);
+    setTimeout(moveLeft, 0);
   } else {
     imagesContainerElement.style.transition = 'transform 1s';
   }
@@ -51,19 +52,48 @@ function moveRight() {
   }
   console.log("Image now:", demonstratingImage);
   imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
-  console.log/("Images container coords:", - demonstratingImage * demonstratorElement.getBoundingClientRect().width);
+  // console.log/("Images container coords:", - demonstratingImage * demonstratorElement.getBoundingClientRect().width);
+  switcherTurnOn(demonstratingImage);
 }
 
-const switcherElements = document.getElementsByClassName("switcher");
-const switcherEvents = [...switcherElements].forEach((switcher, index) => switcher.addEventListener("click", onSwitcher.bind(null, index)));
+[...imageElements].forEach((element, index) => {
+  if (index !== imageElements.length - 1) {
+    createSwitcher("div", "switcher", document.getElementsByClassName("switchers-container")[0]);
+  }
+});
 
+const switcherElements = document.getElementsByClassName("switcher");
+
+[...switcherElements].forEach((switcher, index) => switcher.addEventListener("click", onSwitcher.bind(null, index)));
+
+switcherTurnOn(demonstratingImage);
 
 function onSwitcher(switcherNumber) {
-  console.log("Switcher event:", switcherNumber)
+  // console.log("Switcher event:", switcherNumber)
   demonstratingImage = switcherNumber;
   imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
+  switcherTurnOn(demonstratingImage);
 }
 
 function performAnimation(numberOfImage, widthOfImage) {
   return imagesContainerElement.style.transform = `translate(${- numberOfImage * widthOfImage}px`;
+}
+
+function createSwitcher(tagName, switcherClass, container) {
+  const element = document.createElement(tagName);
+  element.classList.add(switcherClass);
+  container.appendChild(element);
+  return element;
+}
+
+function switcherTurnOn (switcherNumber) {
+  if (switcherNumber === imageElements.length - 1) {
+    switcherNumber = 0;
+  }
+  [...switcherElements].forEach(element => {
+    element.classList.remove("switcherTurnedOn");
+    element.classList.add("switcher");
+  });
+  // console.log("Switcher Element:", switcherElements[switcherNumber]);
+  switcherElements[switcherNumber].classList.add("switcherTurnedOn");
 }
