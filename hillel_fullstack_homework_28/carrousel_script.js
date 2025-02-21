3d7859c85cc24a23736f9270cdec5ffdb1151776
JemmUA <jemm.ua@gmail.com>
2025-02-21 09:55:18 +0200

    refactoring hw27 for hw28

--- hillel_fullstack_homework_28/carrousel_script.js
+++ hillel_fullstack_homework_28/carrousel_script.js
@@ -1,6 +1,7 @@
-let demonstratingImage = 0;
-const filePathImg = "sources/images/";
-const imageFileNames = [
+import {Carrousel} from "./entities/Carrousel.js";
+
+const filePathCars = "sources/images/cars/";
+const imageFileCars = [
   "acura_rdx.jpg",
   "audi_r8_gt.jpg",
   "chevrolet-corvette.jpg",
@@ -17,131 +18,174 @@ const imageFileNames = [
   "renault_captur.jpg",
   "subaru_impreza_22b_sti.jpg"
 ];
+
+const filePathImgMac = "sources/images/mac/";
+const imageFileNamesMac = [
+  "mac_1.jpg",
+  "mac_2.jpg",
+  "mac_3.jpg",
+  "mac_4.jpg",
+  "mac_5.jpg",
+]
+
+
+const filePathAnimals = "sources/images/animals/";
+const imageFileNameAnimals = [
+    "animal_01.jpg",
+    "animal_02.jpg",
+    "animal_03.jpg",
+    "animal_04.jpg",
+    "animal_05.jpg",
+    "animal_06.jpg",
+    "animal_07.jpg",
+    "animal_08.jpg",
+    "animal_09.jpg",
+    "animal_10.jpg"
+    ]
+
+let demonstratingImage = 0;
+let imageElementsAmount = 0;
+let imageWidth;
 let isAutoplaying = false;
 let autoplayingInterval;
 const moveTime = 1;
 
-const moverLeftElement = document.querySelector("#common-container #moverLeft");
-const moverRightElement = document.querySelector("#common-container #moverRight");
-moverLeftElement.addEventListener("click", moveLeft);
-moverRightElement.addEventListener("click", moveRight);
-const demonstratorElement = document.querySelector("#common-container #demonstrator");
-const imagesContainerElement = document.querySelector("#common-container #images-container")
-const autoplayRunElement = document.querySelector("#common-container .run-container");
-autoplayRunElement.addEventListener("click", autoplay);
-const autoplayStopElement = document.querySelector("#common-container .stop-container");
-autoplayStopElement.addEventListener("click", autoplay);
+// const carrousel = new Carrousel(imageFileCars, filePathCars);
 
-generateImgElements();
-const imageElements = document.querySelectorAll("#common-container img");
-function moveRight() {
-  if (demonstratingImage === imageElements.length - 1) {
-    setTimeout(moveRight, 0);
-    imagesContainerElement.style.transition = 'transform 0s';
-  } else {
-    imagesContainerElement.style.transition = 'transform 1s';
-  }
-  if (demonstratingImage  < imageElements.length - 1) {
-    demonstratingImage++;
-  } else {
-    demonstratingImage = 0;
-  }
-  imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
-  switcherTurnOn(demonstratingImage);
-}
+// const carrousel = new Carrousel(imageFileCars, filePathCars);
+// carrousel.test();
 
-function moveLeft() {
-  if (demonstratingImage === 0) {
-    imagesContainerElement.style.transition = 'transform 0s';
-    setTimeout(moveLeft, 0);
-  } else {
-    imagesContainerElement.style.transition = 'transform 1s';
-  }
-  if (demonstratingImage > 0) {
-    demonstratingImage--;
-  } else {
-    demonstratingImage = imageElements.length - 1;
-  }
-  if (demonstratingImage === imageElements.length - 1) {
-  }
-  imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
-  switcherTurnOn(demonstratingImage);
-}
+// const carrousel2 = new Carrousel(imageFileNamesMac, filePathImgMac);
+// carrousel2.test();
+//
+const carrousel3 = new Carrousel(imageFileNameAnimals, filePathAnimals);
+carrousel3.test();
 
-[...imageElements].forEach((element, index) => {
-  if (index !== imageElements.length - 1) {
-    createSwitcherElement("div", "switcher", document.querySelectorAll("#common-container .switchers-container")[0]);
-  }
-});
-
-const switcherElements = document.querySelectorAll("#common-container .switcher");
-
-[...switcherElements].forEach((switcher, index) => switcher.addEventListener("click", onSwitcher.bind(null, index)));
-
-switcherTurnOn(demonstratingImage);
-
-function onSwitcher(switcherNumber) {
-  demonstratingImage = switcherNumber;
-  imagesContainerElement.style.transform = performAnimation(demonstratingImage, demonstratorElement.getBoundingClientRect().width);
-  switcherTurnOn(demonstratingImage);
-}
-
-function performAnimation(numberOfImage, widthOfImage) {
-  return imagesContainerElement.style.transform = `translate(${- numberOfImage * widthOfImage}px`;
-}
-
-function createSwitcherElement(tagName, switcherClass, container) {
-  const element = document.createElement(tagName);
-  element.classList.add(switcherClass);
-  container.appendChild(element);
-  return element;
-}
-
-function switcherTurnOn (switcherNumber) {
-  if (switcherNumber === imageElements.length - 1) {
-    switcherNumber = 0;
-  }
-  [...switcherElements].forEach(element => {
-    element.classList.remove("switcherTurnedOn");
-    element.classList.add("switcher");
-  });
-  switcherElements[switcherNumber].classList.add("switcherTurnedOn");
-}
-
-function generateImgElements() {
-  let imagesForHtml = `<${imagesContainerElement.tagName.toLowerCase()} id="${imagesContainerElement.id}">`;
-  let firstImageForDuplication = "";
-  imageFileNames.forEach((fileNameImg, index) => {
-    if (index === 0) {
-      firstImageForDuplication = `<img src="${filePathImg}${fileNameImg}" title="${fileNameImg.
-      slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
-    }
-    imagesForHtml += `<img src="${filePathImg}${fileNameImg}" title="${fileNameImg.
-    slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
-  });
-    imagesForHtml += firstImageForDuplication;
-  imagesForHtml += "</div>";
-  imagesContainerElement.innerHTML = imagesForHtml;
-}
-
-function autoplay(){
-  isAutoplaying = !isAutoplaying;
-    if (isAutoplaying) {
-      autoplayRunElement.classList.add("deactivated");
-      autoplayRunElement.classList.remove("activated");
-      autoplayStopElement.classList.add("activated");
-      autoplayStopElement.classList.remove("deactivated");
-    } else {
-      autoplayRunElement.classList.remove("deactivated");
-      autoplayRunElement.classList.add("activated");
-      autoplayStopElement.classList.remove("activated");
-      autoplayStopElement.classList.add("deactivated");
-    }
-
-  if (isAutoplaying) {
-    autoplayingInterval = setInterval(moveRight, moveTime * 1000);
-  } else {
-    clearInterval(autoplayingInterval);
-    autoplayingInterval = null;
-  }
-}
+// document.querySelector("#common-container #moverLeft").addEventListener("click", moveLeft);
+// document.querySelector("#common-container #moverRight").addEventListener("click", moveRight);
+// const imagesContainerElement = document.querySelector("#common-container #images-container")
+// const autoplayRunElement = document.querySelector("#common-container .run-container");
+// autoplayRunElement.addEventListener("click", autoplay);
+// const autoplayStopElement = document.querySelector("#common-container .stop-container");
+// autoplayStopElement.addEventListener("click", autoplay);
+//
+// imageWidth =  document.querySelector("#common-container #demonstrator").getBoundingClientRect().width;
+//
+// generateImgElements();
+// const imageElements = document.querySelectorAll("#common-container img");
+// imageElementsAmount = imageElements.length;
+//
+// function moveRight() {
+//   if (demonstratingImage === imageElementsAmount - 1) {
+//     setTimeout(moveRight, 0);
+//     imagesContainerElement.style.transition = 'transform 0s';
+//   } else {
+//     imagesContainerElement.style.transition = 'transform 1s';
+//   }
+//   if (demonstratingImage  < imageElementsAmount - 1) {
+//     demonstratingImage++;
+//   } else {
+//     demonstratingImage = 0;
+//   }
+//   imagesContainerElement.style.transform = performAnimation(demonstratingImage, imageWidth);
+//   switcherTurnOn(demonstratingImage);
+// }
+//
+// function moveLeft() {
+//   if (demonstratingImage === 0) {
+//     imagesContainerElement.style.transition = 'transform 0s';
+//     setTimeout(moveLeft, 0);
+//   } else {
+//     imagesContainerElement.style.transition = 'transform 1s';
+//   }
+//   if (demonstratingImage > 0) {
+//     demonstratingImage--;
+//   } else {
+//     demonstratingImage = imageElementsAmount - 1;
+//   }
+//   if (demonstratingImage === imageElementsAmount - 1) {
+//   }
+//   imagesContainerElement.style.transform = performAnimation(demonstratingImage, imageWidth);
+//   switcherTurnOn(demonstratingImage);
+// }
+//
+// generateSwitchers();
+// function generateSwitchers(){
+//   [...imageElements].forEach((element, index) => {
+//     if (index !== imageElementsAmount - 1) {
+//       createSwitcherElement("div", "switcher", document.querySelectorAll("#common-container .switchers-container")[0]);
+//     }
+//   });
+// }
+//
+// const switcherElements = document.querySelectorAll("#common-container .switcher");
+//
+// [...switcherElements].forEach((switcher, index) => switcher.addEventListener("click", onSwitcher.bind(null, index)));
+//
+// switcherTurnOn(demonstratingImage);
+//
+// function onSwitcher(switcherNumber) {
+//   demonstratingImage = switcherNumber;
+//   imagesContainerElement.style.transform = performAnimation(demonstratingImage, imageWidth);
+//   switcherTurnOn(demonstratingImage);
+// }
+//
+// function performAnimation(numberOfImage, widthOfImage) {
+//   return imagesContainerElement.style.transform = `translate(${- numberOfImage * widthOfImage}px`;
+// }
+//
+// function createSwitcherElement(tagName, switcherClass, container) {
+//   const element = document.createElement(tagName);
+//   element.classList.add(switcherClass);
+//   container.appendChild(element);
+//   return element;
+// }
+//
+// function switcherTurnOn (switcherNumber) {
+//   if (switcherNumber === imageElementsAmount - 1) {
+//     switcherNumber = 0;
+//   }
+//   [...switcherElements].forEach(element => {
+//     element.classList.remove("switcherTurnedOn");
+//     element.classList.add("switcher");
+//   });
+//   switcherElements[switcherNumber].classList.add("switcherTurnedOn");
+// }
+//
+// function generateImgElements() {
+//   let imagesForHtml = `<${imagesContainerElement.tagName.toLowerCase()} id="${imagesContainerElement.id}">`;
+//   let firstImageForDuplication = "";
+//   imageFileNames.forEach((fileNameImg, index) => {
+//     if (index === 0) {
+//       firstImageForDuplication = `<img src="${filePathImg}${fileNameImg}" title="${fileNameImg.
+//       slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
+//     }
+//     imagesForHtml += `<img src="${filePathImg}${fileNameImg}" title="${fileNameImg.
+//     slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
+//   });
+//     imagesForHtml += firstImageForDuplication;
+//   imagesForHtml += "</div>";
+//   imagesContainerElement.innerHTML = imagesForHtml;
+// }
+//
+// function autoplay(){
+//   isAutoplaying = !isAutoplaying;
+//     if (isAutoplaying) {
+//       autoplayRunElement.classList.add("deactivated");
+//       autoplayRunElement.classList.remove("activated");
+//       autoplayStopElement.classList.add("activated");
+//       autoplayStopElement.classList.remove("deactivated");
+//     } else {
+//       autoplayRunElement.classList.remove("deactivated");
+//       autoplayRunElement.classList.add("activated");
+//       autoplayStopElement.classList.remove("activated");
+//       autoplayStopElement.classList.add("deactivated");
+//     }
+//
+//   if (isAutoplaying) {
+//     autoplayingInterval = setInterval(moveRight, moveTime * 1000);
+//   } else {
+//     clearInterval(autoplayingInterval);
+//     autoplayingInterval = null;
+//   }
+// }
---
+++ hillel_fullstack_homework_28/entities/Carrousel.js
@@ -0,0 +1,156 @@
+export class Carrousel {
+    images = [];
+    imageElementsAmount = 0;
+    moveTime = 1;
+
+    demonstratingImage = 0;
+    imageWidth = 0;
+    isAutoplaying = false;
+    autoplayingInterval;
+    imagesContainerElement;
+
+    constructor(images, filePathImg){
+        if(!Array.isArray(images) || !images.length) {
+            throw new Error ("There are no images provided");
+        }
+
+        this.imageElementsAmount = images.length;
+        this.filePathImg = filePathImg;
+        this.images = images;
+
+        this.startEngine();
+    }
+
+    startEngine() {
+        document.querySelector("#common-container #moverLeft").addEventListener("click", this.moveLeft.bind(this));
+        document.querySelector("#common-container #moverRight").addEventListener("click", this.moveRight.bind(this));
+        this.imagesContainerElement = document.querySelector("#common-container #images-container")
+        this.autoplayRunElement = document.querySelector("#common-container .run-container");
+        this.autoplayRunElement.addEventListener("click", this.autoplay.bind(this));
+        this.autoplayStopElement = document.querySelector("#common-container .stop-container");
+        this.autoplayStopElement.addEventListener("click", this.autoplay.bind(this));
+        this.imageWidth = document.querySelector("#common-container #demonstrator").getBoundingClientRect().width;
+        this.generateImgElements();
+        this.imageElements = document.querySelectorAll("#common-container img");
+        this.imageElementsAmount = this.imageElements.length;
+        this.generateSwitchers();
+        this.switcherElements = document.querySelectorAll("#common-container .switcher");
+        // console.log("Images count:", this.switcherElements.length);
+        [...this.switcherElements].forEach((switcher, index) => switcher.addEventListener("click", this.onSwitcher.bind(null, index)));
+        // this.switcherTurnOn(this.demonstratingImage);
+        this.onSwitcher(this.demonstratingImage)
+    }
+
+    moveLeft() {
+        if (this.demonstratingImage === 0) {
+            this.imagesContainerElement.style.transition = 'transform 0s';
+            setTimeout(this.moveLeft.bind(this), 0);
+        } else {
+            this.imagesContainerElement.style.transition = 'transform 1s';
+        }
+        if (this.demonstratingImage > 0) {
+            this.demonstratingImage--;
+        } else {
+            this.demonstratingImage = this.imageElementsAmount - 1;
+        }
+        if (this.demonstratingImage === this.imageElementsAmount - 1) {
+        }
+        this.imagesContainerElement.style.transform = this.performAnimation(this.demonstratingImage, this.imageWidth);
+        this.switcherTurnOn(this.demonstratingImage);
+    }
+
+    moveRight() {
+        if (this.demonstratingImage === this.imageElementsAmount - 1) {
+            setTimeout(this.moveRight.bind(this), 0);
+            this.imagesContainerElement.style.transition = 'transform 0s';
+        } else {
+            this.imagesContainerElement.style.transition = 'transform 1s';
+        }
+        if (this.demonstratingImage  < this.imageElementsAmount - 1) {
+            this.demonstratingImage++;
+        } else {
+            this.demonstratingImage = 0;
+        }
+        this.imagesContainerElement.style.transform = this.performAnimation(this.demonstratingImage, this.imageWidth);
+        this.switcherTurnOn(this.demonstratingImage);
+    }
+
+    generateImgElements() {
+        let imagesForHtml = `<${this.imagesContainerElement.tagName.toLowerCase()} id="${this.imagesContainerElement.id}">`;
+        let firstImageForDuplication = "";
+        this.images.forEach((fileNameImg, index) => {
+            if (index === 0) {
+                firstImageForDuplication = `<img src="${this.filePathImg}${fileNameImg}" title="${fileNameImg.
+                slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
+            }
+            imagesForHtml += `<img src="${this.filePathImg}${fileNameImg}" title="${fileNameImg.
+            slice(0, -4)}" alt="${fileNameImg.slice(0, -4)}">`;
+        });
+        imagesForHtml += firstImageForDuplication;
+        imagesForHtml += "</div>";
+        this.imagesContainerElement.innerHTML = imagesForHtml;
+    }
+
+    generateSwitchers(){
+        [...this.imageElements].forEach((_, index) => {
+            if (index !== this.imageElementsAmount - 1) {
+                this.createSwitcherElement("div", "switcher", document.querySelectorAll("#common-container .switchers-container")[0]);
+            }
+        });
+    }
+
+    onSwitcher(switcherNumber) {
+        console.log("this.demonstratingImage, switcherNumber:", this.demonstratingImage, switcherNumber);
+        this.demonstratingImage = switcherNumber;
+        this.imagesContainerElement.style.transform = performAnimation(this.demonstratingImage, this.imageWidth);
+        switcherTurnOn(this.demonstratingImage);
+    }
+
+    performAnimation(numberOfImage, widthOfImage) {
+        return this.imagesContainerElement.style.transform = `translate(${- numberOfImage * widthOfImage}px`;
+    }
+
+    createSwitcherElement(tagName, switcherClass, container) {
+        const element = document.createElement(tagName);
+        element.classList.add(switcherClass);
+        container.appendChild(element);
+        return element;
+    }
+
+    switcherTurnOn (switcherNumber) {
+        if (switcherNumber === this.imageElementsAmount - 1) {
+            switcherNumber = 0;
+        }
+        [...this.switcherElements].forEach(element => {
+            element.classList.remove("switcherTurnedOn");
+            element.classList.add("switcher");
+        });
+        [...this.switcherElements][switcherNumber].classList.add("switcherTurnedOn");
+    }
+
+    autoplay(){
+        this.isAutoplaying = !this.isAutoplaying;
+        if (this.isAutoplaying) {
+            this.autoplayRunElement.classList.remove("activated");
+            this.autoplayRunElement.classList.add("deactivated");
+            this.autoplayStopElement.classList.remove("deactivated");
+            this.autoplayStopElement.classList.add("activated");
+        } else {
+            this.autoplayRunElement.classList.remove("deactivated");
+            this.autoplayRunElement.classList.add("activated");
+            this.autoplayStopElement.classList.remove("activated");
+            this.autoplayStopElement.classList.add("deactivated");
+        }
+
+        if (this.isAutoplaying) {
+            this.autoplayingInterval = setInterval(this.moveRight.bind(this), this.moveTime * 1000);
+        } else {
+            clearInterval(this.autoplayingInterval);
+            this.autoplayingInterval = null;
+        }
+    }
+
+    test() {
+        console.log("Test. Demo image number:", this.demonstratingImage);
+    }
+}
