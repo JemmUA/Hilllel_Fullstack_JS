7e20321bb142184e82ad333a1d72c62551d16cf9
JemmUA <jemm.ua@gmail.com>
2025-03-17 17:31:55 +0200

    hw33 assets/styles ok

--- hillel_fullstack_homework_33/gulpfile.js
+++ hillel_fullstack_homework_33/gulpfile.js
@@ -1,11 +1,24 @@
 const {src, dest, task, watch} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
+const cssnano = require('cssnano');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const plugins = [cssnano({ preset: 'default' })];
+
 // console.log("GULP!", gulp);
 
-function scss() {
+// const PATH = {
+//
+// }
+
+
+function scssMin() {
     return src("./src/**/*.scss")
         .pipe(sass()).on("error", sass.logError)
+        .pipe(postcss(plugins))
+        .pipe(rename({ suffix: '.min' }))
+        // .pipe(dest("./assets"));
         .pipe(dest("./assets/styles"));
 }
 
@@ -23,7 +36,7 @@ async function sync() {
 
 function watchFiles() {
     syncInit();
-    watch("./src/**/*.scss", scss);
+    watch("./src/**/*.scss", scssMin);
     watch("./src/**/*.scss", sync);
     watch("./*.html", sync);
     watch("./src/**/*.js", sync);
