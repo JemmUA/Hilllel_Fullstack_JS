6ad5423b2855b4b5f3c30f8a4cc73484c89da3f7
JemmUA <jemm.ua@gmail.com>
2025-03-20 02:57:57 +0200

    hw32. very begining

--- hillel_fullstack_homework_32/movies.js
+++ hillel_fullstack_homework_32/movies.js
@@ -1,4 +1,11 @@
 // console.log("Movie?");
+const API_KEY = '29f0bbd3';
+const API_URL = 'https://www.omdbapi.com/';
+
 
 const searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", () => console.log("Movie?"));
+let inputMovie = document.getElementById("inputMovie");
+searchButton.addEventListener("click", () => {
+    console.log("Movie?");
+    console.log("і?", inputMovie.value);
+});
