d4af24e0a20ec522fee51472604d7860dc42eb48
JemmUA <jemm.ua@gmail.com>
2025-03-20 08:04:53 +0200

    hw 32 searching ok

--- hillel_fullstack_homework_32/movies.js
+++ hillel_fullstack_homework_32/movies.js
@@ -3,9 +3,23 @@ const API_KEY = '29f0bbd3';
 const API_URL = 'https://www.omdbapi.com/';
 
 
-const searchButton = document.getElementById("searchButton");
+const searchButtonElement = document.getElementById("searchButton");
 let inputMovie = document.getElementById("inputMovie");
-searchButton.addEventListener("click", () => {
-    console.log("Movie?");
-    console.log("і?", inputMovie.value);
+searchButtonElement.addEventListener("click", async () => {
+    console.log("Movie?", inputMovie.value);
+    const foundMovie = await searchMovie(inputMovie.value);
+    console.log("Found Movie!:", foundMovie);
 });
+
+async function searchMovie(requestString) {
+    const searchLink = (`${API_URL}?apikey=${API_KEY}&s=${requestString}`);
+    const dataMovie = await fetch(searchLink)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error ("Відповідь на запит невдала: ", response);
+            } else {
+                return response.json();
+            }
+        });
+        return dataMovie?.Search;
+    }
