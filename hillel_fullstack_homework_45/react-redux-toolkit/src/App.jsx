f8e128e5fd7b21bd3f99fbeb102a0f93ca7ca1ab
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-04-25 13:35:34 +0300

    hw45 very beginning

---
+++ hillel_fullstack_homework_45/react-redux-toolkit/src/App.jsx
@@ -0,0 +1,51 @@
+import {createContext} from 'react'
+import './App.css'
+import CheckBox from "./components/CheckBox.jsx";
+import ControlledNick from "./components/ControlledNick.jsx";
+import UncontrolledNickEvent from "./components/UncontrolledNickEvent.jsx";
+import HomeworkHeader from "./components/HomeworkHeader.jsx";
+import Plate from "./components/Plate.jsx";
+import User, {storeValue} from "./components/User.jsx";
+import {Provider} from "react-redux";
+
+export const CheckNickRuleContext = createContext();
+
+function App() {
+
+  // RegEx змінної checkNickRule використовується в компонентах:
+  //    ControlledNick
+  //    UncontrolledNickEvent
+  // та передається в них через контекст CheckNickRuleContext
+  const checkNickRule = /^[a-z A-Z 0-9]{8,16}$/;
+
+  return (
+    <>
+        <div>
+            <HomeworkHeader/>
+            <CheckNickRuleContext.Provider value={checkNickRule}>
+              <Plate>
+                  <ControlledNick/><br/>
+                  <UncontrolledNickEvent/>
+              </Plate>
+            </CheckNickRuleContext.Provider>
+            <Plate>
+              <div id="checkBoxContainer">
+                  <CheckBox >AAA</CheckBox>
+                  <CheckBox >BBB</CheckBox>
+                  <CheckBox >CCC</CheckBox>
+                  <CheckBox >DDD</CheckBox>
+                  <CheckBox >EEE</CheckBox>
+              </div>
+            </Plate>
+            <Plate>
+              <Provider store={storeValue}>
+                <User></User>
+              </Provider>
+            </Plate>
+        </div>
+
+    </>
+  )
+}
+
+export default App;
---
+++ hillel_fullstack_homework_45/react-redux-toolkit/src/components/Plate.jsx
@@ -0,0 +1,11 @@
+import "../css/plate.css"
+
+function Plate(props) {
+    return (
+        <div className="plate">{props.children}
+        </div>
+    );
+
+}
+
+export default Plate;
