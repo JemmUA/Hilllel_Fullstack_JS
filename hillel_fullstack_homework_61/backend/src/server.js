a3f40432b83d3039c26d82de8156d9a35793e6b2
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-06-30 17:56:18 +0300

    hw61 ok

--- hillel_fullstack_homework_61/backend/src/server.js
+++ hillel_fullstack_homework_61/backend/src/server.js
@@ -9,25 +9,35 @@ console.log(articles);
 app.set('view engine', 'pug');
 app.get('/articles', (req, res) => {
   let articlesText = '';
-  articles.forEach((art, id) => articlesText += art.title + '\n' + art.text + '<br>');
-  const data = {title: 'PUG', artTitle: "Articles", artText: articlesText};
-  res.status(200).render('index', data);
+  articles.forEach(art => articlesText += art.title + ' <br> ' + art.text + '<br> <br> ');
+  const data = {title: 'PUG | All articles', articlesPageTitle: "Статті", articles: articlesText};
+  res.status(200).render('articles', data);
 });
 
 app.get('/articles/:id', (req, res) => {
     const articleId = Number(req.params.id);
     console.log(articleId);
 
-  if (!(isNaN(articleId)) &&
-      (articles.find((art) => art.id === articleId).id === articleId)) {
+  if (
+    !(isNaN(articleId))
+    // ||
+    &&
+  // (articles.find((art) => art.id === articleId).id === articleId)
+    (articleId > 0 && articleId < 10)
+  )
+  {
         console.log(`article id #${articleId} found`);
         const data = {
           title: 'PUG',
           artTitle: articles[articleId - 1 ].title,
-          artText: articles[articleId - 1 ].text};
+          artText: articles[articleId - 1 ].text
+        };
         res.status(200).render('index', data);
       } else {
-       res.status(404).render('', 'Wrong article\'s id');
+        const error = 'Панікуйте вже - 404 ;))';
+        console.error(error);
+        const data = {title: 'PUG | Error', error: error};
+        res.status(404).render('error404', data);
   }
 
 });
