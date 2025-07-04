69547b9fdd4e7ad466bb756613876f0536321b51
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-07-04 15:47:27 +0300

    hw61 added export/import

--- hillel_fullstack_homework_61/backend/src/server.js
+++ hillel_fullstack_homework_61/backend/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {articles, getArticleById, getAllArticles, error404} from "./models/articles.js";
+import {articles, getArticleById, getAllArticles, error404} from "../models/articles.js";
 
 const PORT = 3039;
 const app = express();
@@ -26,31 +26,6 @@ app.get('/articles/:id', (req, res) => {
       error404(req, res);
   }
 });
-
-// const getAllArticles = ((req,res, articles) => {
-//     let articlesText = '';
-//     articles.forEach(art => articlesText += art.title + ' <br> ' + art.text + '<br> <br> ');
-//     const data = {title: 'PUG | All articles', articlesPageTitle: "Статті", articles: articlesText};
-//     res.status(200).render('articles', data);
-// });
-//
-// const getArticleById = ((req, res, articles, articleId) => {
-//     console.log(`article id #${articleId} found`);
-//     const data = {
-//         title: 'PUG',
-//         artTitle: articles[articleId - 1].title,
-//         artText: articles[articleId - 1].text
-//     }
-//     res.status(200).render('index', data);
-//     });
-
-// const error404 = (req, res) => {
-//     const error = 'Панікуйте вже - 404 ;))';
-//     const error = 'Панікуйте вже - 404 ;))';
-//     console.error(error);
-//     const data = {title: 'PUG | Error', error: error};
-//     res.status(404).render('error404', data);
-// }
 
 app.listen(PORT, () => {
   console.log(`express server at port: ${PORT}`);
