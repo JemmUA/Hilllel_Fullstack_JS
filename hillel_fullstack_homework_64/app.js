cb3c674ee729c07ab69399829d94f7d341bb33da
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-07-10 23:39:57 +0300

    hw64

--- hillel_fullstack_homework_64/app.js
+++ hillel_fullstack_homework_64/app.js
@@ -22,12 +22,19 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   const collections = await currentDatabase.listCollections().toArray();
-  let collectionsHTML = '<h1>Collections</h1>';
+  let collectionsHTML = `<p>Database:</p> <h1>${CURR_DB_NAME}</h1><h2>Collections</h2><hr>`;
     collections.forEach(collection => {
-      collectionsHTML += '<a href="/${collection}/">${collection}</a><br>';
+      collectionsHTML += `<h3><a href="/${collection.name}/">${collection.name}</a></h3>`;
   });
   res.send(collectionsHTML);
   // res.send('<a href="/movies/">Movies</a>');
+});
+
+app.get('/users', async (req, res) => {
+  const usersCollection = currentDatabase.collection('users'); // Object moviesCollection for working with the collection
+  console.log('Users Collection:', usersCollection);
+  const users = await usersCollection.find().skip(1).limit(3).toArray();
+  res.json(users);
 });
 
 app.get('/movies', async (req, res) => {
@@ -36,6 +43,48 @@ app.get('/movies', async (req, res) => {
   const movies = await moviesCollection.find().skip(1).limit(2).toArray();
   // res.send('You will not believe :))) It works!!!!!')
   res.json(movies);
+});
+
+app.get('/embedded_movies', async (req, res) => {
+  const embeddedMoviesCollection = currentDatabase.collection('embedded_movies'); // Object moviesCollection for working with the collection
+  console.log('Embedded movies Collection:', embeddedMoviesCollection);
+  const embeddedMovies = await embeddedMoviesCollection.find().skip(3).limit(1).toArray();
+  res.json(embeddedMovies);
+});
+
+app.get('/sessions', async (req, res) => {
+  const sessionsCollection = currentDatabase.collection('sessions'); // Object moviesCollection for working with the collection
+  console.log('Sessions Collection:', sessionsCollection);
+  const sessions = await sessionsCollection.find().limit(1).toArray();
+  res.json(sessions);
+});
+
+app.get('/theaters', async (req, res) => {
+  const theatersCollection = currentDatabase.collection('theaters'); // Object moviesCollection for working with the collection
+  console.log('Theaters Collection:', theatersCollection);
+  const theaters = await theatersCollection.find().skip(1).limit(2).toArray();
+  res.json(theaters);
+});
+
+app.get('/comments', async (req, res) => {
+  const commentsCollection = currentDatabase.collection('comments'); // Object moviesCollection for working with the collection
+  console.log('Comments Collection:', commentsCollection);
+  const comments = await commentsCollection.find().skip(1).limit(3).toArray();
+  res.json(comments);
+});
+
+
+app.get('/users/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  if (!ObjectId.isValid(userId)) {
+    return res.status(400).send('Wrong user Id format');
+  }
+
+  const usersCollection = currentDatabase.collection('users'); // Object moviesCollection for working with the collection
+  console.log('Users Collection:', usersCollection);
+  const users = await usersCollection.find({ _id: new ObjectId(userId)}).toArray();
+  res.json(users);
 });
 
 app.get('/movies/:movieId', async (req, res) => {
@@ -51,6 +100,8 @@ app.get('/movies/:movieId', async (req, res) => {
   const movies = await moviesCollection.find({ _id: new ObjectId(movieId)}).toArray();
   res.json(movies);
 });
+
+
 
 async function startServer() {
   console.log('Starting server...!');
