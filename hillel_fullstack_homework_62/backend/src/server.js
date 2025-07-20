ec686e1e56a652b2b38c9a380b0cd7f93c8c23b7
Oleksandr Omelyan <jemm.ua@gmail.com>
2025-07-20 16:14:35 +0300

    hw 62 register err

--- hillel_fullstack_homework_62/backend/src/server.js
+++ hillel_fullstack_homework_62/backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import {articles, getArticleById, getAllArticles, error404} from "../models/articles.js";
 import cookieParser from 'cookie-parser';
+// import bcrypt from 'bcrypt';
 
 const PORT = 3039;
 const app = express();
+
+const users = [];
 
 console.log(articles);
 
@@ -11,7 +14,11 @@ app.set('view engine', 'pug');
 
 app.use(cookieParser());
 
+app.use(express.urlencoded({ extended: true}));
+
 app.use(express.static('public'));
+
+// GET
 
 app.get('/', (req, res) => {
   res.send(createHtmlPage('Main page', `
@@ -21,6 +28,21 @@ app.get('/', (req, res) => {
     <h4><a href="/pages">Demo of static files</a></h4>
     <h4><a href="/secured">Secured page</a></h4>
 `));
+});
+
+app.get('/registration', (req, res) => {
+  res.send(createHtmlPage('Registration', `
+    <form method="post" action="/registration">
+      <input name="username" required placeholder="name"/>
+      <input name="password" type="password" required placeholder="pass"/>
+      <button>Register</button>
+    </form>
+    `));
+});
+
+app.get('/secured', (req, res) => {
+  res.send(createHtmlPage('Secured page', "Interesting content :)"))
+
 });
 
 app.get('/articles', (req, res) => {
@@ -41,6 +63,29 @@ app.get('/articles/:id', (req, res) => {
       error404(req, res);
   }
 });
+
+// POST
+app.post('/registration', async (req, res) => {
+  console.log('username: ', username);
+  console.log('password: ', password);
+
+  if (!username || !password) {
+    return res.status(400).send('Username and Password are required for registration');
+  }
+
+  if (users.find(user => user.username === username)) {
+    return res.status(400).send('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(5);
+  const hashPass = await bcrypt.hash(password, salt);
+
+  users.push({username, hashPass});
+  res.send('Success. User registered.');
+});
+
+
+//
 
 function checkAuthorisation(req, res, next) {
   console.log('Cookies:', req.cookies.token);
